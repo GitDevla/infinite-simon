b227bebfcedf7e4ae2c71e47059b7814d73cf25d
Pataki Dávid Ferenc <pataki.david.ferenc@gmail.com>
2025-09-22 19:42:06 +0200

    feat: implement routing with GameScreen component

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './style/index.css';
-import App from './page/App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./style/index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import App from "./page/App";
+import GameScreen from "./page/GameScreen";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+	document.getElementById("root") as HTMLElement,
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<App />} />
+				<Route path="/game" element={<GameScreen />} />
+			</Routes>
+		</BrowserRouter>
+	</React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
