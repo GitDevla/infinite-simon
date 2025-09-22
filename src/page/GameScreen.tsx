bcc77cdba0dbbdbe540ae930805444c884bbee6e
Pataki Dávid Ferenc <pataki.david.ferenc@gmail.com>
2025-09-22 20:07:09 +0200

    feat: add basic elements to GameScreen

--- src/page/GameScreen.tsx
+++ src/page/GameScreen.tsx
@@ -1,3 +1,35 @@
+import Button from "../component/Button";
+import Slider from "../component/Slider";
+import Switch from "../component/Switch";
+
 export default function GameScreen() {
-	return <div>Game Screen</div>;
+	return (
+		<div>
+			<div>
+				<h1>Game Screen</h1>
+				<span>Score: 0</span>
+			</div>
+
+			<div>
+				<div>
+					<Button color="green" />
+					<Button color="red" />
+					<Button color="blue" />
+					<Button color="yellow" />
+				</div>
+				<div>
+					<Slider />
+				</div>
+				<div>
+					<Switch />
+					<Switch />
+				</div>
+				<div>
+					{/* Placeholder for knobs */}
+					<Slider max={8} />
+					<Slider max={8} />
+				</div>
+			</div>
+		</div>
+	);
 }
