8f62303f599342354405c80bb4eecb1c388ba7c8
Pataki Dávid Ferenc <pataki.david.ferenc@gmail.com>
2025-09-23 16:02:47 +0200

    refactor: comment out React.StrictMode in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -10,14 +10,14 @@ const root = ReactDOM.createRoot(
 	document.getElementById("root") as HTMLElement,
 );
 root.render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<App />} />
-				<Route path="/game" element={<GameScreen />} />
-			</Routes>
-		</BrowserRouter>
-	</React.StrictMode>,
+	// <React.StrictMode>
+	<BrowserRouter>
+		<Routes>
+			<Route path="/" element={<App />} />
+			<Route path="/game" element={<GameScreen />} />
+		</Routes>
+	</BrowserRouter>,
+	// </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
