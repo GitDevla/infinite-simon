8f11faf01336c8dea35cd8e99c806502ffc1a12b
Csendes Dávid <csndsdvd@mailbox.unideb.hu>
2025-09-26 12:11:33 +0200

    Finalized starting screen

--- src/page/App.tsx
+++ src/page/App.tsx
@@ -6,8 +6,15 @@ import { BrowserRouter, Link } from 'react-router-dom';
 function App() {
   return (
     <nav>
-      <div className='wrapper'>
+      <div className='wrapper'> 
         <table>
+          <div className='title'>
+            <span style={{color:"#ca1515ff"}}>IN</span>
+            <span style={{color:"#1e8b1eff"}}>FI</span>
+            <span style={{color:"#4123c5ff"}}>NI</span>
+            <span style={{color:"#fffc3dff"}}>TE </span>
+            simon
+          </div>
           <tr>
             <a href='/game'><div className='navButton'>Play!</div></a>
           </tr>
