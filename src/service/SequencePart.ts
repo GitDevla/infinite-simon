bc50bb32908ad164f0089cfdef1ed1d93076abf9
Pataki Dávid Ferenc <pataki.david.ferenc@gmail.com>
2025-09-27 17:26:15 +0200

    feat: change id property from private to public in SequencePart class

--- src/service/SequencePart.ts
+++ src/service/SequencePart.ts
@@ -1,6 +1,6 @@
 export abstract class SequencePart{
     public type: string;
-    private id: string;
+    public id: string;
     public expectedValue: any;
 
     constructor(type: string, id: string, exectedValue: any){
