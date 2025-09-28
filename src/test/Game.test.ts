5de8ec3eb14cd06526b19112cb9f26ff138eb2bb
Kerekes Bence <162814033+bence255@users.noreply.github.com>
2025-09-28 19:19:36 +0200

    test: add unit test for Game.ts

---
+++ src/test/Game.test.ts
@@ -0,0 +1,36 @@
+import { Game } from "../service/Game";
+import { KnobPart } from "../service/Parts";
+
+describe("Game class test", () => {
+    let game: Game;
+
+    beforeEach(() => {
+        game = new Game();
+        game.startNewGame();
+    });
+
+    test("New game start", () => {
+        expect(game.getCurrentRound()).toBe(1);
+        expect(game.isGameOver()).toBe(false);
+        expect(game.getSequence().getParts().length).toBe(1)
+    });
+
+    test("advance to next round on correct input", () => {
+        const part = game.getSequence().getCurrentPart();
+        const result = game.checkPlayerInput(part);
+
+        expect(result).toBe(true);
+        expect(game.getCurrentRound()).toBe(2)
+    });
+
+    test("game over on incorrect input", () => {
+        const incorrectInput = new KnobPart();
+        const result = game.checkPlayerInput(incorrectInput);
+
+        expect(game.isGameOver()).toBe(true);
+        expect(result).toBe(false);
+        expect(game.getCurrentRound()).toBe(1)
+    });
+});
+
+    
