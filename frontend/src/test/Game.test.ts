a461824c9c840907ad142d7a6700980dca507e93
Kerekes Bence <162814033+bence255@users.noreply.github.com>
2025-10-13 19:37:01 +0200

    test: new game tpyes

--- frontend/src/test/Game.test.ts
+++ frontend/src/test/Game.test.ts
@@ -1,35 +1,45 @@
 import {Game} from "../service/Game";
 import {KnobPart} from "../service/Parts";
+import { GameType } from '../service/Game';
 
-describe("Game class test", () => {
-	let game: Game;
+const gameConfigs = Object.values(GameType)
+  .filter((v) => typeof v === 'number')
+  .map((value) => ({
+    label: `${GameType[value]} Game`,
+    gameType: value as GameType
+  }));
 
-	beforeEach(() => {
-		game = new Game();
-		game.startNewGame(1);
-	});
+gameConfigs.forEach(({ label, gameType }) => {
+  	describe(`Game class test - ${label}`, () => {
+		let game: Game;
 
-	test("New game start", () => {
-		expect(game.getCurrentRound()).toBe(1);
-		expect(game.isGameOver()).toBe(false);
-		expect(game.getSequence().getParts().length).toBe(1);
-	});
+		beforeEach(() => {
+			game = new Game();
+			game.startNewGame(1,gameType);
+		});
 
-	test("advance to next round on correct input", () => {
-		const part = game.getSequence().getCurrentPart();
-		const result = game.checkPlayerInput(part);
+		test("New game start", () => {
+			expect(game.getCurrentRound()).toBe(1);
+			expect(game.isGameOver()).toBe(false);
+			expect(game.getSequence().getParts().length).toBe(1);
+		});
 
-		expect(result).toBe(true);
-		expect(game.getCurrentRound()).toBe(2);
-	});
+		test("advance to next round on correct input", () => {
+			const part = game.getSequence().getCurrentPart();
+			const result = game.checkPlayerInput(part);
 
-	test("game over on incorrect input", () => {
-		const rng = () => 0.5;
-		const incorrectInput = new KnobPart(rng);
-		const result = game.checkPlayerInput(incorrectInput);
+			expect(result).toBe(true);
+			expect(game.getCurrentRound()).toBe(2);
+		});
 
-		expect(game.isGameOver()).toBe(true);
-		expect(result).toBe(false);
-		expect(game.getCurrentRound()).toBe(1);
+		test("game over on incorrect input", () => {
+			const rng = () => 0.5;
+			const incorrectInput = new KnobPart(rng);
+			const result = game.checkPlayerInput(incorrectInput);
+
+			expect(game.isGameOver()).toBe(true);
+			expect(result).toBe(false);
+			expect(game.getCurrentRound()).toBe(1);
+		});
 	});
 });
